2f8c76673c5f3bf6db811b0adaf7bdc17e2aca19
Anders <Anders@BATTLESTATION>
2015-11-02 21:38:43 +0100

    Interface

--- src/contract/DataRepository.java
+++ src/contract/DataRepository.java
@@ -1,14 +1,28 @@
+package contract;
+
+import DTO.Customer;
+import DTO.Departure;
+import DTO.Reservation;
+import DTO.Route;
+import java.util.List;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
 
-package contract;
-
 /**
  * @author Anders
  */
 public interface DataRepository {
-    
+    public List<Reservation> getReservations(int customerId);
+    public Reservation getReservation(int reservationId);
+    public boolean makeReservation(Reservation reservation);
+    public boolean changeReservation(Reservation reservation);
+    public List<Route> getAllRoutes();
+    public Route getRoute(int routeId);
+    public List<Departure> getSchedule();
+    public Departure getDeparture(int departureId);
+    public Customer getCustomer(int customerId);
 }
